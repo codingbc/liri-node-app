880c332de69942fab794cd9b805ffec412bf5978
Nida <codingbc@gmail.com>
2018-06-19 20:21:32 -0400

    upload

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: 'g1zHVMAXeHpsa0OXqSXdXFM4h',
+  consumer_secret: '7E3LmLtG9YDgJZ7WXwMEUsNWTn8QBcLJsNZAytm1RsDlQxqiqh',
+  access_token_key: '1008886768152825856-9JlSdIOmrKfmr8E9NOPs4lQPhTM9uC',
+  access_token_secret: '4LIbvTVMw7O5AZjCHuK55RZbkhqiktuYA1SLJReSJCt9i'
+};
+
+exports.spotify = {
+  id: '11ab0c43bc7e4a52985fab37d3f97bd6',
+  secret: '04d1d9e2c9a744d096ef26b0d52cee16',
+};
