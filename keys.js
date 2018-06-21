9bf3f93a80c6b9ad29ab66b5e0f7b8f6cf9ab77b
Nida <codingbc@gmail.com>
2018-06-20 22:38:54 -0400

    update

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: 'g1zHVMAXeHpsa0OXqSXdXFM4h',
-  consumer_secret: '7E3LmLtG9YDgJZ7WXwMEUsNWTn8QBcLJsNZAytm1RsDlQxqiqh',
-  access_token_key: '1008886768152825856-9JlSdIOmrKfmr8E9NOPs4lQPhTM9uC',
-  access_token_secret: '4LIbvTVMw7O5AZjCHuK55RZbkhqiktuYA1SLJReSJCt9i'
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: '11ab0c43bc7e4a52985fab37d3f97bd6',
-  secret: '04d1d9e2c9a744d096ef26b0d52cee16',
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
